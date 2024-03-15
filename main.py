05e8785d31a06fdae6716d1c6badc93574d29b4e
sean15535 <sean15535@gmail.com>
2024-03-15 17:49:10 +0100

    fixed the generate password function

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import random
+import string
 
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + "!@#$%^&*()-_+=[]{}|;:,.<>?"
+    return ''.join(random.choice(characters) for _ in range(length))
 
+def user():
+    name = input("Your name: ")
+    password = input("Enter password: ")
+    special_characters = "!@#$%^&*()-_+=[]{}|;:,.<>?"
+    lower = any(x.islower() for x in password)
+    upper = any(x.isupper() for x in password)
+    digits = any(x.isdigit() for x in password)
+    character = any(char in special_characters for char in password )
+    new_password = generate_password(8)
 
-name = input("Your name: ")
-password = input("Enter password: ")
-
-special_characters = "!@#$%^&*()-_+=[]{}|;:,.<>?"
-
-if len(password) >= 8 and any(x.islower() for x in password) and any(x.isupper() for x in password) and any(char in special_characters for char in password):
-    print(f"{name} {password}")
-else:
-    print("Password is weak.\nThis is your New Password: ")
-
+    if len(password) >= 8 and lower and upper and character and digits:
+        print(f"{name} {password}")
+    else:
+        print(f"Password is weak.\nTry using this password: {new_password} ")
+user()
